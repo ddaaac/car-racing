7037ba0b390a4f53cd223940984b415d2195d4b0
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-01 15:29:37 +0900

    Update dqn.py

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -22,9 +22,9 @@ class DQN(nn.Module):
         self.linear2 = nn.Linear(256, num_of_actions)
 
     def forward(self, x):
-        conv1_out = F.relu(self.batch_norm1(self.max_pool1(self.conv1(x))))
-        conv2_out = F.relu(self.batch_norm2(self.conv2(conv1_out)))
-        conv3_out = F.relu(self.batch_norm3(self.conv3(conv2_out)))
+        conv1_out = F.relu(self.batch_norm1(self.max_pool1(self.conv1_2(self.conv1_1(x)))))
+        conv2_out = F.relu(self.batch_norm2(self.conv2_2(self.conv2_1(conv1_out))))
+        conv3_out = F.relu(self.batch_norm3(self.conv3_2(self.conv3_1(conv2_out))))
 
         flattened = torch.flatten(conv3_out, start_dim=1)
         linear1_out = self.linear1(flattened)
