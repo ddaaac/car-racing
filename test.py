a70bf3ef33bb0466e0969a2e44d812572d7c69ff
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-01 12:17:33 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+https://drive.google.com/open?id=1uPpYYPZ2pmUvbNf5ndOLkTvZDvx_XYPb
+
+https://drive.google.com/open?id=1ge7K3s__hYKFplu8xpDY2ufqWh4a6WFn
