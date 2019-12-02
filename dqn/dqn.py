a1a04ead5789859f1e704f4c81bb18fa76c1fafd
BEOMJUN KIM <skyblue300a@gmail.com>
2019-12-02 22:58:16 +0900

    Revert "Update dqn.py"
    This reverts commit c0445ab45fa1fc1571a6133f7667dccdb13481e4.

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -7,21 +7,24 @@ class DQN(nn.Module):
     def __init__(self, input_shape, num_of_actions):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(input_shape, 16, kernel_size=5, stride=2)
+        self.conv1_1 = nn.Conv2d(input_shape, 16, kernel_size=3, stride=2, padding=1)
+        self.conv1_2 = nn.Conv2d(16, 16, kernel_size=3, stride=1)
         self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
         self.batch_norm1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.conv2_1 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.conv2_2 = nn.Conv2d(32, 32, kernel_size=3, stride=1)
         self.batch_norm2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.conv3_1 = nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
+        self.conv3_2 = nn.Conv2d(32, 32, kernel_size=3, stride=1)
         self.batch_norm3 = nn.BatchNorm2d(32)
 
         self.linear1 = nn.Linear(288, 256)
         self.linear2 = nn.Linear(256, num_of_actions)
 
     def forward(self, x):
-        conv1_out = F.relu(self.batch_norm1(self.max_pool1(self.conv1(x))))
-        conv2_out = F.relu(self.batch_norm2(self.conv2(conv1_out)))
-        conv3_out = F.relu(self.batch_norm3(self.conv3(conv2_out)))
+        conv1_out = F.relu(self.batch_norm1(self.max_pool1(self.conv1_2(self.conv1_1(x)))))
+        conv2_out = F.relu(self.batch_norm2(self.conv2_2(self.conv2_1(conv1_out))))
+        conv3_out = F.relu(self.batch_norm3(self.conv3_2(self.conv3_1(conv2_out))))
 
         flattened = torch.flatten(conv3_out, start_dim=1)
         linear1_out = self.linear1(flattened)
@@ -37,3 +40,5 @@ if __name__ == '__main__':
     m = a.max(1)[1]
     print(a)
     print(m)
+
+
